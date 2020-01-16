bb5868da59d15288501ce737e360f324d0a8c7e2
mohbadar <mohammadbadarhashimi@gmail.com>
2020-01-16 10:42:45 +0430

    anar framewok and kafka connect integration module

---
+++ src/main/java/af/gov/anar/connect/AnarConnectApplication.java
@@ -0,0 +1,13 @@
+package af.gov.anar.connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnarConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnarConnectApplication.class, args);
+	}
+
+}
